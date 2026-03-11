2994cf94527c8ca9aacaa9f98e7efafd15e9ad11
princeodoh566 <princeodoh566@gmail.com>
2026-03-11 12:36:27 +0100

    Create App.jsx in the src/ folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// This file is created to hold the App component
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the StreetKash MVP!</h1>
+        </div>
+    );
+};
+
+export default App;
